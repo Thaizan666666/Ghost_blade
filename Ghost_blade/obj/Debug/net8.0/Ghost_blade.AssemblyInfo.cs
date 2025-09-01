1dd0ea3b0ddcd04ab671faaf5a61db5f1f8efa50
Thaizan666666 <Thaizan246810@gmail.com>
2025-09-01 15:00:44 +0700

    gitignore
    gitignore

--- Ghost_blade/obj/Debug/net8.0/Ghost_blade.AssemblyInfo.cs
+++ Ghost_blade/obj/Debug/net8.0/Ghost_blade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f278b5f0d4961ccee353105078dbd0b0a7edbc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57fb173c1a31aa24bc0b90d1e1f02a6478fa462")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
