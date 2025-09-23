65bb00a5f53bf7e63eef189279fad7448baa7514
Thaizan666666 <Thaizan246810@gmail.com>
2025-09-23 15:57:51 +0700

    New Big Update Version 0.7.5

--- Ghost_blade/obj/Debug/net8.0/Ghost_blade.AssemblyInfo.cs
+++ Ghost_blade/obj/Debug/net8.0/Ghost_blade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94da835d98b5f04177716a07df2bf08853fc645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174d93f73b1c3a78cd9ed3e9e227917f32656857")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
