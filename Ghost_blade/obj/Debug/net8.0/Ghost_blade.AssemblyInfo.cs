47939d27ff5af41c7db70ca5e8523a2bbdb1a60c
Thaizan666666 <Thaizan246810@gmail.com>
2025-10-06 00:15:20 +0700

    Update_to_marge
    Update_to_marge

--- Ghost_blade/obj/Debug/net8.0/Ghost_blade.AssemblyInfo.cs
+++ Ghost_blade/obj/Debug/net8.0/Ghost_blade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0edc3e705eea9a05a99ad49b637533456bf245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023a86c645af6a094591a54ad085974dc9887526")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ghost_blade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
